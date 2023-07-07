ea5d33a5e29e474673a8cafc1d9dcc1ddf054ed0
AC1802 <jacantos.jac@gmail.com>
2023-07-07 16:27:29 +0200

    Architecture du code amélioré
    - Ajout des dossiers pour les composants, layouts...
    
    - Création des routes

---
+++ my-app/src/routes/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from '../pages/Home/Home';
+import About from '../pages/About/About';
+import Housing_sheet from '../pages/Housing_sheet/Housing_sheet';
+import Error from '../pages/Error/Error';
+import reportWebVitals from '../reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/housing/:id" element={<Housing_sheet />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
