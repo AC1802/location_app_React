ef5a74c7574102b03dec236abea422a72b432228
AC1802 <jacantos.jac@gmail.com>
2023-07-11 10:09:51 +0200

    Header stylisé

--- my-app/src/layouts/Header.js
+++ my-app/src/layouts/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Logo from  '../assets/images/logo.png';
 import {Link} from 'react-router-dom';
+import HeaderCSS from '../styles/Header.module.css';
 
 export default function Header() {
     return (
-        <nav>
+        <nav className={HeaderCSS.navbar}>
             <div className="logo_container">
-                <img src={Logo} alt="logo"/>
+                <img src={Logo} alt="logo" className={HeaderCSS.logo}/>
             </div>
             <div className="list">
-                <Link to="/" className="list__items">Accueil</Link>
-                <Link to="/about" className="list__items">A Propos</Link>
+                <Link to="/" className={HeaderCSS.list_items}>Accueil</Link>
+                <Link to="/about" className={HeaderCSS.list_items}>A Propos</Link>
             </div>
         </nav>
     )
