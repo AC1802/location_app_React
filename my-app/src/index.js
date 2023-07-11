52c92b2fd249de8b3c6c8639ed68b4118956aae4
AC1802 <jacantos.jac@gmail.com>
2023-07-11 08:20:33 +0200

    Mise a jour de l'architecture du code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Home from '../pages/Home/Home';
-import About from '../pages/About/About';
-import Housing_sheet from '../pages/Housing_sheet/Housing_sheet';
-import Error from '../pages/Error/Error';
-import reportWebVitals from '../reportWebVitals';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import HousingSheet from './pages/Housing_sheet/Housing_sheet';
+import Error from './pages/Error/Error';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +14,7 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/housing/:id" element={<Housing_sheet />} />
+        <Route path="/housing/:id" element={<HousingSheet />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
