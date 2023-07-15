ad6be039a21c0c3ecbeb2be1f483648d482cc4e3
AC1802 <jacantos.jac@gmail.com>
2023-07-15 15:31:08 +0200

    Mis à jour de la page home

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import Banner from '../../components/Banner/Banner';
 import Card from '../../components/Card/Card';
 import '../../styles/styles.css';
 import BackgroundImage from '../../assets/images/banner.png'
+import Locations from '../../back/logements.json'
 
 let bannerTitle = "Chez vous, partout et ailleurs";
 
@@ -11,7 +13,15 @@ export default function Home() {
         <div className="main-container">
             <Banner title={bannerTitle} backgroundImage={BackgroundImage}/>
             <div className="card-container">
-                <Card />
+                {
+                    Locations.map( location => {
+                        return(
+                            <Link to="">
+                                <Card id={location.id} cover={location.cover} title={location.title} />
+                            </Link>
+                        )
+                    })
+                }
             </div>
         </div>
     )
