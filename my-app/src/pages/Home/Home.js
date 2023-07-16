7c985d75025a38a5ee7d8e59ec76033c431d6570
AC1802 <jacantos.jac@gmail.com>
2023-07-16 21:25:35 +0200

    Utilisation de fetch au lieu de import pour les données

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,28 +1,40 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-import Banner from '../../components/Banner/Banner';
-import Card from '../../components/Card/Card';
-import '../../styles/styles.css';
-import BackgroundImage from '../../assets/images/banner.png'
-import Locations from '../../back/logements.json'
-
-let bannerTitle = "Chez vous, partout et ailleurs";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card";
+import "../../styles/styles.css";
+import BackgroundImage from "../../assets/images/banner.png";
 
 export default function Home() {
-    return (
-        <div className="main-container">
-            <Banner title={bannerTitle} backgroundImage={BackgroundImage}/>
-            <div className="card-container">
-                {
-                    Locations.map( location => {
-                        return(
-                            <Link to="">
-                                <Card id={location.id} cover={location.cover} title={location.title} />
-                            </Link>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
+  const bannerTitle = "Chez vous, partout et ailleurs";
+  const [locationData, setLocationData] = useState([]);
+
+  useEffect(() => {
+    fetch(`./logements.json`)
+      .then((response) => response.json())
+      .then((locationData) => setLocationData(locationData))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="mainContainer">
+      <Banner title={bannerTitle} backgroundImage={BackgroundImage} />
+      <div className="cardContainer">
+        <ul className="cardContainer_ul">
+          {locationData.map((location) => {
+            return (
+              <li key={location.id}>
+                <Link to="/housing/${location.id}">
+                  <Card
+                    cover={location.cover}
+                    title={location.title}
+                  />
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
