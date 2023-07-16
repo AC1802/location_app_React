7bd5df77b4becb33b5556441a1d962bfb744f38b
AC1802 <jacantos.jac@gmail.com>
2023-07-16 21:25:58 +0200

    Update Card.js

--- my-app/src/components/Card/Card.js
+++ my-app/src/components/Card/Card.js
@@ -4,7 +4,6 @@ import styles from "../../styles/Card.module.css";
 export default function Card(props) {
   return (
     <article
-      key={props.id}
       className={styles.article}
       style={{
         backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(255, 255, 255, 0.00) 100%), url(${props.cover})`,
