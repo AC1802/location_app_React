ee9c37c748292068ac788eb5eb27cf043203023b
AC1802 <jacantos.jac@gmail.com>
2023-07-17 23:35:45 +0200

    Début d'implémentation de la page logement

---
+++ my-app/src/pages/HousingSheet/HousingSheet.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+// import Carrousel from "../../components/Carrousel/Carrousel";
+import styles from "../../styles/HousingSheet.module.css";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import Tag from "../../components/Tag/Tag";
+import Rating from "../../components/Rating/Rating";
+
+export default function HousingSheet() {
+  const { id } = useParams();
+  const [locationData, setLocationData] = useState([]);
+
+  useEffect(() => {
+    fetch(`../logements.json`)
+      .then((response) => response.json())
+      .then((locationData) => setLocationData(locationData))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const currentLocation = locationData.find((location) => location.id === id);
+  console.log(currentLocation);
+
+  return (
+    <div className="mainContainer">
+      <div className={styles.mainContainerInner}>
+        <div>
+          <h1 className={styles.h1}>{currentLocation?.title}</h1>
+          <h2 className={styles.h2}>{currentLocation?.location}</h2>
+          <div className={styles.tagContainer}>
+            {currentLocation?.tags.map((tag, index) => {
+              return <Tag key={index} tag={tag} />;
+            })}
+          </div>
+        </div>
+        <div className={styles.hostRatingContainer}>
+          <div className={styles.hostContainer}>
+            <p className={styles.hostName}>{currentLocation?.host?.name}</p>
+            <div className={styles.hostPictureContainer}>
+              <img className={styles.hostPicture} src={currentLocation?.host?.picture} />
+            </div>
+          </div>
+          <div>{Rating(currentLocation?.rating)}</div>
+        </div>
+      </div>
+      <div  className={styles.dropdownContainer}>
+        <Dropdown title="Description" text={currentLocation?.description} />
+        <Dropdown
+          title="Équipements"
+          text={currentLocation?.equipments.map((equipment, index) => {
+            return (
+              <p key={index}>
+                {equipment} {"\n"}
+              </p>
+            );
+          })}
+        />
+      </div>
+    </div>
+  );
+}
