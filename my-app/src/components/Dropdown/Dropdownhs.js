e77bcdede1fc7c069a9b31f864b0556ebe667042
AC1802 <jacantos.jac@gmail.com>
2023-07-18 09:15:56 +0200

    Correction du composant dropdown

--- my-app/src/components/Dropdown/Dropdownhs.js
+++ my-app/src/components/Dropdown/Dropdownhs.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "../../styles/Dropdown.module.css";
+import styles from "../../styles/Dropdownhs.module.css";
 
 export default function Dropdown(props) {
   const [isToggle, setIsToggle] = useState(false);
@@ -28,7 +28,7 @@ export default function Dropdown(props) {
         </div>
       </div>
       <div className={styles.pContainer}>
-        <p className={styles.p}>{props.text}</p>
+        <div className={styles.p}>{props.text}</div>
       </div>
     </div>
   ) : (
@@ -47,7 +47,7 @@ export default function Dropdown(props) {
             viewBox="0 0 32 32"
             fill="none"
           >
-            <g clipPath="url(#clip0_181350_179)">
+            <g clip-path="url(#clip0_181350_179)">
               <path
                 d="M14.7897 22.2103C15.4591 22.8797 16.5462 22.8797 17.2157 22.2103L27.4979 11.928C28.1674 11.2586 28.1674 10.1715 27.4979 9.50206C26.8285 8.83265 25.7414 8.83265 25.072 9.50206L16 18.574L6.92804 9.50742C6.25862 8.838 5.17148 8.838 4.50206 9.50742C3.83265 10.1768 3.83265 11.264 4.50206 11.9334L14.7843 22.2157L14.7897 22.2103Z"
                 fill="white"
